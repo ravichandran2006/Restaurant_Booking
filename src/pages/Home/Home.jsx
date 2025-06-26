1195cfdbe8f42feb274843bf4990a42a1be955b9
ravichandran2006 <ravichandranm.23aim@kongu.edu>
2025-06-26 18:16:53 +0530

    home page added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to TasteBite Restaurant</h1>
+      <p>Book your table online. Quick. Easy. Delicious.</p>
+      <img
+        src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5"
+        alt="Restaurant"
+        className="home-image"
+      />
+    </div>
+  );
+}
+
+export default Home;
