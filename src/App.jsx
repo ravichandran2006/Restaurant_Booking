68bbf954bd885e8800c3c64ed24d2a640cf2cca6
ravichandran2006 <ravichandranm.23aim@kongu.edu>
2025-06-27 18:04:55 +0530

    install react router dom and about is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/navbar';
 import Home from './pages/Home/Home';
+import About from './components/About/about';
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   );
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -8,11 +8,11 @@ function Navbar() {
         <img src={food_logo} alt="Akshaya Bhavan" />
       </div>
       <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Signin</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#signin">Signin</a></li>
       </ul>
     </nav>
   );
