4a5a9de9dc5e050ec44fd9667c0a2cd56094f0de
pramod-k31 <pramodk.23aim@kongu.edu>
2025-06-28 11:03:32 +0530

    footer files is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar/navbar';
 import Home from './pages/Home/Home';
 import About from './components/About/about';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Footer />
     </>
   );
 }
